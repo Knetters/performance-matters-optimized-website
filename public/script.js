d0793e98010f3c2ea447a85a1a8b7e866d9f2ca9
Tjips <thomas@scheepers.com>
2023-04-25 13:02:35 +0200

    Min plus button working (plus button moet verborgen worden)

--- public/script.js
+++ public/script.js
@@ -2,36 +2,40 @@
 var countDownDate = new Date().getTime() + 40*60*1000
 var x = setInterval(function() {
 
-  var now = new Date().getTime()
-  var distance = countDownDate - now
+    var now = new Date().getTime()
+    var distance = countDownDate - now
 
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-  if (minutes < 10) { minutes = "0" + minutes; }
-  if (seconds < 10) { seconds = "0" + seconds; }
+    if (minutes < 10) { minutes = "0" + minutes; }
+    if (seconds < 10) { seconds = "0" + seconds; }
 
-  document.getElementById("timer").innerHTML = minutes + ":" + seconds
+    // Zet de minuten en seconden in de html
+    document.getElementById("timer").innerHTML = minutes + ":" + seconds
 
-  if (distance <= 0) {
-    clearInterval(x)
-    document.getElementById("timer").innerHTML = "0:00"
-  }
+    if (distance <= 0) {
+        clearInterval(x)
+        document.getElementById("timer").innerHTML = "0:00"
+    }
 
 }, 1000)
 
 // Quote panel toggelen
 const quoteBlock = document.getElementById("quote-block")
-const quoteButton = document.getElementById("quote-button")
+const plusButton = document.getElementById("plus-button")
 
-document.getElementById("min-button").addEventListener("click", toggleBlock);
+// Min knop function
+document.getElementById("min-button").addEventListener("click", toggleBlockOff);
 
-function toggleBlock() {
-  quoteBlock.classList.toggle("d-none")
+function toggleBlockOff() {
+    quoteBlock.classList.toggle("d-none")
+    plusButton.classList.toggle("d-block")
 }
 
-document.getElementById("quote-button").addEventListener("click", toggleBlock);
+document.getElementById("plus-button").addEventListener("click", toggleBlockOn);
 
-function toggleBlock() {
-  quoteBlock.classList.toggle("d-none")
+// Plus knop function
+function toggleBlockOn() {
+    quoteBlock.classList.toggle("d-none")
 }
